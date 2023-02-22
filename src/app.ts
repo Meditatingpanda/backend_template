8d9a65f800640b5190a05bacca6bc785c5f4709b
Gyana Ranjan Panda <grp.gyanaranjan@gmail.com>
2023-02-22 19:48:29 +0530

    setup done

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
-import express from "express";
-
+import express, { Request, Response } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import colors from "colors";
+import fs from "fs";
+import https from "https";
+import path from "path";
+import morgan from "morgan";
 const app = express();
 const port = process.env.PORT || 3000;
+const credentials = {
+  key: fs.readFileSync(path.join(__dirname, "./certificates/key.pem"), "utf8"),
+  cert: fs.readFileSync(path.join(__dirname, "./certificates/cert.pem"), "utf8"),
+};
+const server = https.createServer(credentials, app);
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(port, () => {
+  console.log(colors.yellow(`Server running on port ${port}`));
 });
+// app.listen(port, () => {
+//   console.log(colors.yellow(`Server running on port ${port}`));
+// });
